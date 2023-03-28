1e39c74eb09e8280f1c70955443ae7e3b944ac52
Eduardo Sanchez Pascual <77649457+Lalo-SP@users.noreply.github.com>
2023-03-27 22:27:20 -0600

    Se agrega el código par-impar.c
    Se agrega el código par-impar.c y se hace cambios en hola.c

--- hola.c
+++ hola.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char const *argv[])
+{
   printf("Hola Mundo GitHub");
   
   return 0;
---
+++ par-impar.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[]) 
+{
+	printf("Contador de numeros Pares e Impares\n");
+
+	return 0;
+}
