2037ba0b5e8a3bed8eee8da37c68e339b22a4d40
Eduardo Sanchez Pascual <77649457+Lalo-SP@users.noreply.github.com>
2023-04-04 21:49:12 -0500

    Cambios en par-impar.c
    Se hicieron cambios en el código

--- par-impar.c
+++ par-impar.c
@@ -11,26 +11,28 @@ int main(int argc, char const *argv[])
 	contImpar = 0;
 
 	printf("Contador de numeros Pares e Impares\n");
-	printf("Ingresa %d numeros positivos\n");
+	printf("Ingresa %d numeros positivos\n", N);
 
 	for (i = 0; i < N; i++) {
 
 		scanf("%d", &numero);
 
-		if(numero > 0) {
+		if(numero >= 0) {
 
 			if(numero % 2 == 0)
 				contPar = contPar + 1;
 			else
 				contImpar = contImpar + 1;
 
-
 		} 
 		else {
-			printf("El numero %d no es positivo\n", numero);
+			printf("El numero %d no es positivo, ingresa de nuevo otro numero\n", numero);
 			i--;
 		}
 	}
+
+	printf("\nEl numero de numeros pares es: %d\n", contPar);
+	printf("\nEl numero de numeros impares es: %d\n", contImpar);
 
 	return 0;
 }
