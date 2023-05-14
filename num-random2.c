f3b28538922ec94b426c6662afc7cfd91de6748b
Eduardo Sanchez Pascual <77649457+LaloSP-dev@users.noreply.github.com>
2023-05-13 19:00:53 -0500

    Update num-random2.c
    Se codifico los métodos Iniciaiza, Despliega y Cuenta

--- num-random2.c
+++ num-random2.c
@@ -4,8 +4,46 @@
 
 #define NUM_ELEMS 40
 
+typedef char cadena[256];
+
 int main(int argc, char const *argv[])
 {
-	
+	int Numeros[NUM_ELEMS];
+
+	srand(time(NULL));
+
 	return 0;
 }
+
+void Inicializa_arreglo(int *Arreglo, int size, int min_valor, int max_valor)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		Arreglo[i] = (rand() + min_valor) % max_valor;
+}
+
+void Despliega_arreglo(int *Arreglo, int size, cadena mensaje)
+{
+	int i;
+
+	printf("%s\n", mensaje);
+
+	for (i = 0; i < size; i++)
+		printf("%d ", Arreglo[i]);
+
+}
+
+int Cuenta(int *Arreglo, int size, int numero)
+{
+	int i, cont;
+
+	cont = 0;
+
+	for (i = 0; i < size; i++)
+		if(Arreglo[i] = numero)
+			cont = cont + 1;
+
+	return cont;
+}
+
