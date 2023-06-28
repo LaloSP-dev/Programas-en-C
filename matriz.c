d66c0ece9555963272f107ffe52452a88c5a054f
Eduardo Sanchez Pascual <77649457+LaloSP-dev@users.noreply.github.com>
2023-06-28 00:04:15 -0500

    Update matriz.c

--- matriz.c
+++ matriz.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 float **crea_matriz_float(int rengs, int cols);
+float *crea_arreglo_float(int tamanio);
 void liberar_matriz_float(float **apmat, int rengs);
-float rand_float_entre_0_y_1(int num_dec)
+float rand_float_entre_0_y_1(int num_dec);
+void Inicializa_aleatorio(float **matriz, int rengs, int cols);
+void Despliega_matriz(float **matriz, int rengs, int cols);
+void Suma_renglones(float **Matriz, float *Arreglo, int rengs, int cols);
+void Despliega_suma_arreglo(float *Arreglo, int size);
 
 int main(int argc, char const *argv[])
 {
+	float **Matriz;
+	float *Arreglo;
+	int rengs, cols;
+
+	srand(time(NULL));
+
+	printf("Ingresa el tama%co de la matriz:\n", 164);
+	printf("Renglones: ");
+	scanf("%d", &rengs);
+	printf("Columnas: ");
+	scanf("%d", &cols);
 	
+	Matriz = crea_matriz_float(rengs, cols);
+	Inicializa_aleatorio(Matriz, rengs, cols);
+	Despliega_matriz(Matriz, rengs, cols);
+
+	Arreglo = crea_arreglo_float(rengs);
+
+	Suma_renglones(Matriz, Arreglo, rengs, cols);
+	Despliega_suma_arreglo(Arreglo, rengs);
+
+
+
 	return 0;
 }
 
@@ -27,7 +55,7 @@ float **crea_matriz_float(int rengs, int cols)
 
 	if(ap_elems != NULL) {
 
-		apmat = (float **)malloc(sizeof(float*)*rengs);
+		apmat = (float **)malloc(sizeof(float *)*rengs);
 
 		if(apmat != NULL)
 			for(r = 0; r < rengs; r++)
@@ -35,6 +63,18 @@ float **crea_matriz_float(int rengs, int cols)
 	}
 
 	return apmat;
+}
+
+float *crea_arreglo_float(int tamanio)
+{
+	float *ap_elems;
+
+	ap_elems = (float *)malloc(sizeof(float *)*tamanio);
+
+	if(ap_elems == NULL)
+		printf("ERROR_memoria insuficiente");
+
+	return ap_elems;
 }
 
 void liberar_matriz_float(float **apmat, int rengs)
@@ -55,11 +95,11 @@ considerando cifras decimales
 float rand_float_entre_0_y_1(int num_dec)
 {
 	float decimales = 0.0;
-	int j, nd = 1;
+	int i, nd = 1;
 
 	if(num_dec > 0) {
 
-		for(j = 1; j <= num_dec; j++)
+		for(i = 1; i <= num_dec; i++)
 			nd = nd * 10;
 
 		decimales = (rand() % (nd + 1)) / (float) nd;
@@ -67,4 +107,52 @@ float rand_float_entre_0_y_1(int num_dec)
 	}
 
 	return decimales;
+}
+
+void Inicializa_aleatorio(float **Matriz, int rengs, int cols)
+{
+	int i, j;
+
+	for(i = 0; i < rengs; i++)
+		for(j = 0; j < cols; j++)
+			Matriz[i][j] = rand_float_entre_0_y_1(1) + (rand() % 10);
+}
+
+void Despliega_matriz(float **Matriz, int rengs, int cols)
+{
+	int i, j;
+
+	printf("\n--------------------------------\n");
+
+	for(i = 0; i < rengs; i++) {
+		
+		for(j = 0; j < cols; j++)
+			printf("%5.1f", Matriz[i][j]);
+		printf("\n");
+	
+	}
+}
+
+void Suma_renglones(float **Matriz, float *Arreglo, int rengs, int cols)
+{
+	int i, j;
+	float suma;
+
+	suma = 0;
+
+	for(i = 0; i < rengs; i++) {
+		for(j = 0; j < cols; j++)
+			suma = suma + Matriz[i][j];
+		Arreglo[i] = suma;
+		suma = 0;
+	}
+}
+
+void Despliega_suma_arreglo(float *Arreglo, int size)
+{
+	int i;
+
+	printf("\n");
+	for(i = 0; i < size; i++)
+		printf("%5.1f", Arreglo[i]);
 }
