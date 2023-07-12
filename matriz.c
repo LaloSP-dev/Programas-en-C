df24f00f4b64cae2d969f1950377861881ddd98e
Eduardo Sanchez Pascual <77649457+LaloSP-dev@users.noreply.github.com>
2023-07-12 00:07:22 -0500

    update matriz.c

--- matriz.c
+++ matriz.c
@@ -3,13 +3,17 @@
 #include <time.h>
 
 float **crea_matriz_float(int rengs, int cols);
+float *crea_arreglo_float(int size);
 float rand_float_entre_0_y_1(int num_dec);
 void Inicializa_Aleatorio(float **Matriz, int rengs, int cols);
 void Despliega_Matriz(float **Matriz, int rengs, int cols);
+void suma_renglones(float **Matriz, float *Arreglo, int rengs, int cols);
+void Despliega_Arreglo(float *Arreglo, int size);
 
 int main(int argc, char const *argv[])
 {
 	float **Matriz;
+	float *Arreglo;
 	int rengs, cols;
 
 	srand(time(NULL));
@@ -23,6 +27,10 @@ int main(int argc, char const *argv[])
 	Matriz = crea_matriz_float(rengs, cols);
 	Inicializa_Aleatorio(Matriz, rengs, cols);
 	Despliega_Matriz(Matriz, rengs, cols);
+
+	Arreglo = crea_arreglo_float(rengs);
+	suma_renglones(Matriz, Arreglo, rengs, cols);
+	Despliega_Arreglo(Arreglo, rengs);
 
 	return 0;
 }
@@ -54,6 +62,24 @@ float **crea_matriz_float(int rengs, int cols)
 	}
 
 	return apmat;
+}
+
+/**
+ * @brief Pide memoria al sistema para crear un arreglo de tipo int
+ *
+ * @param size
+ * @return float*
+ */
+float *crea_arreglo_float(int size)
+{
+	float *ap_elems;
+
+	ap_elems = (float *)malloc(sizeof(float) * size);
+
+	if (ap_elems == NULL)
+		printf("ERROR...memoria insuficiente\n");
+
+	return ap_elems;
 }
 
 /**
@@ -99,4 +125,40 @@ void Despliega_Matriz(float **Matriz, int rengs, int cols)
 		printf("\n");
 	}
 	printf("\n");
+}
+
+/**
+ * @brief Suma de renglones de la Matriz
+ *
+ * Guarda cada entrada r del arreglo la suma del reglon i-ésimo
+ * de la matriz
+ *
+ * @param Matriz
+ * @param Arreglo
+ * @param rengs
+ * @param cols
+ */
+void suma_renglones(float **Matriz, float *Arreglo, int rengs, int cols)
+{
+	int r, c;
+	float suma = 0.0;
+
+	for (r = 0; r < rengs; r++)
+	{
+		for (c = 0; c < cols; c++)
+			suma = suma + Matriz[r][c];
+		Arreglo[r] = suma;
+		suma = 0.0;
+	}
+}
+
+void Despliega_Arreglo(float *Arreglo, int size)
+{
+	int i;
+
+	printf("\n");
+	for (i = 0; i < size; i++)
+		printf("%5.1f ", Arreglo[i]);
+
+	printf("\n\n");
 }
